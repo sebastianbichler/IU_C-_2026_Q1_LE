27c132423c654a93e0b17a06252e286087c8d50c
Sebastian Bichler <sebastian.bichler@iu.org>
2026-01-06 17:35:27 +0100

    Playground

---
+++ src/Demo.Console/Launcher.cs
@@ -0,0 +1,10 @@
+﻿using Demo.Console.Game01;
+
+public class Launcher
+{
+    public static void Main(string[] args)
+    {
+        var app = new MyApp();
+        app.Run();
+    }
+}
---
+++ src/Playground/Launcher.cs
@@ -0,0 +1,8 @@
+﻿namespace Playground;
+/*
+ * Abstract base class for all playground launchers.
+ */
+public abstract class Launcher
+{
+    public abstract void Run();
+}
