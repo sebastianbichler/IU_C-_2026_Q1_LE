d92e6e101da36054c52c67814def0e754a3c8fd8
b9benign <morkovwastaken@gmail.com>
2026-01-13 11:02:38 +0100

    Add LeapYear class with IsLeapYear method implementation

---
+++ src/Playground/E01G02/LeapYear.cs
@@ -0,0 +1,12 @@
+class LeapYear
+{
+    public static bool IsLeapYear(int year)
+    {
+        if (year % 400 == 0)
+            return true;
+        else if (year % 4 == 0 && year % 100 != 0)
+            return true;
+        else
+            return false;
+    }
+}
