3ea51e8df29f74841b4cc42ce08cf41881b834a0
zichao.cai <zichao.cai@iu-study.org>
2026-01-26 12:59:01 +0100

    Übung 4 span

--- src/Playground/E01G02/Program.cs
+++ src/Playground/E01G02/Program.cs
@@ -5,6 +5,39 @@ public class Program : Launcher
     public override void Run()
     {
         Console.Clear();
-        Console.WriteLine("Put your code for Exercise 1 here.");
+
+        string data = "ID:42;TYPE:PLAYER;POS:10,20";
+        Console.WriteLine($"Data: {data}\n");
+
+        ReadOnlySpan<char> span = data.AsSpan();
+
+        var idValue = ExtractValue(span, "ID:");
+        int id = int.Parse(idValue);
+        Console.WriteLine($"ID: {id}");
+
+        var typeValue = ExtractValue(span, "TYPE:");
+        string type = typeValue.ToString();
+        Console.WriteLine($"TYPE: {type}");
+
+        var posValue = ExtractValue(span, "POS:");
+        int commaIndex = posValue.IndexOf(',');
+        int x = int.Parse(posValue.Slice(0, commaIndex));
+        int y = int.Parse(posValue.Slice(commaIndex + 1));
+        Console.WriteLine($"POS: X={x}, Y={y}");
+
     }
+
+    private ReadOnlySpan<char> ExtractValue(ReadOnlySpan<char> span, string key)
+    {
+        int keyIndex = span.IndexOf(key.AsSpan());
+        if (keyIndex == -1) return ReadOnlySpan<char>.Empty;
+
+        int startIndex = keyIndex + key.Length;
+        var reminder = span.Slice(startIndex);
+
+        int semicolonIndex = reminder.IndexOf(';');
+
+        return semicolonIndex == -1 ? reminder : reminder.Slice(0, semicolonIndex);
+    }
+
 }
